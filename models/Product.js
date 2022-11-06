203237d34b8417a47f3a92a25374046d7e27ccbe
mubarek78 <70493771+mubarek78@users.noreply.github.com>
2022-11-05 20:56:43 -0600

    payment integration with stripe backend complited

--- models/Product.js
+++ models/Product.js
@@ -2,16 +2,24 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
+    labels: { type: String, required: true},
     title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true, },
+    description: { type: String, required: true, },
     img: { type: String, required: true },
-    categories: { type: Array },
+    hoverImg: { type: String},
+    categories: { type: Array, required: true},
     size: { type: String },
     color: { type: String },
+    group: { type: String },
+    inStock: { type: Boolean, required: true},
+    fastDelivery: { type: Boolean },
     price: { type: Number, required: true },
+    rating: { type: Number, required: true },
     
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("Product", ProductSchema);
+
+
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,21 +1,38 @@
+
 const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-router.post("/payment", (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+router.post("/payment", async (req, res) => {
+  // console.log(req.body)
+  // stripe.charges.create(
+  //   {
+  //     source: req.body.tokenId,
+  //     amount: req.body.amount,
+  //     currency: "usd",
+  //   },
+  //   (stripeErr, stripeRes) => {
+  //     if (stripeErr) {
+  //       res.status(500).json(stripeErr);
+  //     } else {
+  //       res.status(200).json(stripeRes);
+  //     }
+  //   }
+  // );
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 2000,
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${process.env.YOUR_DOMAIN}/success`,
+    cancel_url: `${process.env.YOUR_DOMAIN}/success`,
+  });
+
+  res.redirect(303, session.url);
 });
 
 module.exports = router;
